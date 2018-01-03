2706c5e37821cc09926860b7a169439f9afaadae
ericchiu <ericjack7@gmail.com>
2018-01-03 13:08:06 +0800

    3

--- app/src/main/java/com/example/student/l2018010202/MainActivity.java
+++ app/src/main/java/com/example/student/l2018010202/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.student.l2018010202;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     CheckBox cb;
     Switch sw;
     ProgressBar pb,pb2;
+    SeekBar sb;
+    TextView tv;
+    Button bu4,bu3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
         sw=(Switch)findViewById(R.id.switch1);
         pb=findViewById(R.id.progressBar);
         pb2=findViewById(R.id.progressBar2);
+        sb=(SeekBar)findViewById(R.id.seekBar);
+        tv=findViewById(R.id.textView);
+        bu4=findViewById(R.id.button4);
+        bu3=findViewById(R.id.button3);
         cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
@@ -50,8 +61,29 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tv.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        bu4.setOnClickListener(this);
+        bu3.setOnClickListener(this);
     }
-/*
+
+    /*
     public void click3(View v)
     {
         pb2.setProgress(pb2.getProgress()-10);
@@ -60,7 +92,8 @@ public class MainActivity extends AppCompatActivity {
     public void click4(View v)
     {
         pb2.setProgress(pb2.getProgress()+10);
-    }*/
+    }
+    */
 
     public void click1(View v)
     {
@@ -82,6 +115,8 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
         }
+
+        Toast.makeText(MainActivity.this,String.valueOf(sb.getProgress()),Toast.LENGTH_SHORT).show();
 
     }
 
@@ -107,6 +142,31 @@ public class MainActivity extends AppCompatActivity {
           }
 
         }.start();
+
+    }
+
+    public void click5(View v)
+    {
+        Button b5=findViewById(R.id.button5);
+        Intent in=new Intent();
+        in.setClass(MainActivity.this,Main2Activity.class);
+        startActivity(in);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.button3:
+                pb2.setProgress(pb2.getProgress()-10);
+                break;
+            case R.id.button4:
+                pb2.setProgress(pb2.getProgress()+10);
+                break;
+        }
+
+
 
     }
 }
